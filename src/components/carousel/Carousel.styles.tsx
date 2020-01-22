1c4873073b30b64a8164e69393b186804df1a2d9
Joe Cowton <joecowton@gmail.com>
2020-01-22 17:24:34 +0000

    feat(carousel): fix carousel styling

---
+++ src/components/carousel/Carousel.styles.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import { device } from '../../styles/media';
+
+export const Main = styled.div`
+  background-color: ${props => props.theme.colors.white};
+  height: 100%;
+  position: relative;
+
+  &:hover {
+    cursor: grab;
+  }
+
+  .slick-slider {
+    width: 100%;
+  }
+
+  .MuiSvgIcon-root {
+    opacity: 0;
+    color: ${props => props.theme.colors.black};
+    background: ${props => props.theme.colors.white};
+    border-radius: 50%;
+
+    @media ${device.md} {
+      font-size: 4rem;
+      opacity: 1;
+    }
+  }
+`;
+
+export const MainInner = styled.div`
+  ${props => props.theme.gridStyles.getContainerStyles()}
+  display: flex;
+  justify-content: center;
+`;
+
+export const Dots = styled.div`
+  &.slick-dots li button::before {
+    font-size: 12px;
+  }
+`;
+
+export const NextArrow = styled.div`
+  position: absolute;
+  z-index: 100;
+  top: calc(50% - 20px);
+  right: 20px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const PrevArrow = styled.div`
+  position: absolute;
+  z-index: 100;
+  top: calc(50% - 20px);
+  left: 20px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const Image = styled.img`
+  padding: 10px 0 0;
+
+  img {
+    height: 100%;
+    width: 100%;
+  }
+`;
---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Slider from 'react-slick';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+
+import imageOne from '../../assets/image1.png';
+import imageTwo from '../../assets/image2.png';
+import imageThree from '../../assets/image3.png';
+
+import * as S from './Carousel.styles';
+
+interface Props {
+  onClick?: () => void;
+}
+
+export default () => {
+  const NextArrow = ({ onClick }: Props) => (
+    <S.NextArrow onClick={onClick}>
+      <NavigateNextIcon />
+    </S.NextArrow>
+  );
+
+  const PrevArrow = ({ onClick }: Props) => (
+    <S.PrevArrow onClick={onClick}>
+      <NavigateBeforeIcon />
+    </S.PrevArrow>
+  );
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+  };
+
+  return (
+    <S.Main>
+      <S.MainInner>
+        <Slider {...settings}>
+          <S.Image src={imageOne} />
+          <S.Image src={imageTwo} />
+          <S.Image src={imageThree} />
+        </Slider>
+      </S.MainInner>
+    </S.Main>
+  );
+};
